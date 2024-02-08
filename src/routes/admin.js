983b7c886abbaddebe44f0ec049cf2763f0469ce
Hedy88 <diab.hedy@gmail.com>
2024-02-08 17:38:02 +0000

    too much to list

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,13 +1,22 @@
 import express from "express";
 import * as middleware from "../utils/middleware.js";
+import { User } from "../models/user.js";
+import { Post } from "../models/post.js"
 
 const router = express.Router();
 
 router.get("/admin", ...middleware.adminOnly, async (req, res, next) => {
     try {
-        res.render("admin", {
-            csrfToken: req.csrfToken(),
-        });
+      const stats = {
+        registeredUsers: await User.countDocuments(),
+        postsCreated: await Post.countDocuments(),
+        administrators: await User.countDocuments({ isAdmin: true })
+      };
+
+      res.render("admin", {
+        csrfToken: req.csrfToken(),
+        stats
+      });
     } catch (error) {
         next(error);
     }
